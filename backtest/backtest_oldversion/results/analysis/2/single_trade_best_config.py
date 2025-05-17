2b6a39d2f8f7a93658deff442f75145d5ac5552e
Private Erhan Cetin <berhancetin@gmail.com>
2025-05-17 19:47:59 +0200

    TASK-1 add env file config parameter removed old version of backtest

--- backtest/backtest_oldversion/results/analysis/2/single_trade_best_config.py
+++
@@ -1,8 +0,0 @@
-#📌 3. En İyi Tek Bir İşlem Yapan Config’ler
-#Amaç: Az işlem ama yüksek kar sağlayan config’leri bul.
-import pandas as pd
-df = pd.read_csv("batch_results.csv")
-winners = df[df["total_gain_usd"] > 0]
-low_trade_winners = winners[winners["total_trades"] < 5]
-print("🎯 Az sayıda işlemle kazandıran config_id'ler:")
-print(low_trade_winners[["config_id", "total_gain_usd", "total_trades"]])
